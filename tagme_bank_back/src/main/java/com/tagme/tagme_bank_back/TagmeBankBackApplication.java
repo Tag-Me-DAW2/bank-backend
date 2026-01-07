4976a960a704569585e15ae4924ab748f6754ad0
Borja10100 <borjatm2004@gmail.com>
2026-01-07 09:18:37 +0100

    init

---
+++ tagme_bank_back/src/main/java/com/tagme/tagme_bank_back/TagmeBankBackApplication.java
@@ -0,0 +1,13 @@
+package com.tagme.tagme_bank_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagmeBankBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagmeBankBackApplication.class, args);
+	}
+
+}
---
+++ tagme_bank_back/src/test/java/com/tagme/tagme_bank_back/TagmeBankBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tagme.tagme_bank_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagmeBankBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
