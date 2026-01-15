81c9add2813fb3fa74bc84f1e57dbdd9ecd87aa7
Javier Serrano Gomez <javier.sego04@gmail.com>
2026-01-15 11:25:17 +0100

    Fix(): Arreglado que faltaba un @ en un filter y añadido CorsFilter

--- src/main/java/com/tagme/tagme_bank_back/web/filter/AuthFilter.java
+++ src/main/java/com/tagme/tagme_bank_back/web/filter/AuthFilter.java
@@ -10,7 +10,7 @@ import org.springframework.stereotype.Component;
 import java.io.IOException;
 
 @Component
-@Order(1)
+@Order(2)
 public class AuthFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
--- src/main/java/com/tagme/tagme_bank_back/web/filter/LoginFilter.java
+++ src/main/java/com/tagme/tagme_bank_back/web/filter/LoginFilter.java
@@ -4,10 +4,12 @@ import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
-@Order(0)
+@Component
+@Order(1)
 public class LoginFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
